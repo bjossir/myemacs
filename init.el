c76318a2a2d5328116eac5ee62a269bab5fda2ba
Bjorn <bjossir@gmail.com>
2017-07-09 11:14:06 +0200

    Did add go-mode from melpa

--- init.el
+++ init.el
@@ -35,6 +35,9 @@
 ;; load my c/c++ stuff
 (load "my-c-stuff")
 
+;; load my go stuff
+(load "my-go-stuff")
+
 ;; load my pascal stuff
 (load "my-pascal-stuff")
 
@@ -54,7 +57,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (lua-mode))))
+ '(package-selected-packages (quote (go-mode lua-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
