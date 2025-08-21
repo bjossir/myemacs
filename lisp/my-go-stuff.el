ab8a8271dc992db1c74f01277923fdacb05fa751
Bjorn Sveinbjornsson <bjossir@gmail.com>
2025-08-21 10:34:21 +0200

    Update my-go-stuff.el

--- lisp/my-go-stuff.el
+++ lisp/my-go-stuff.el
@@ -2,28 +2,15 @@
 ;;; Commentary:
 ;;; Code:
 
-(setenv "GOPATH"
-        (concat (getenv "HOME") "/gocode"))
-
-;; The PATH variable is used by the emacs shell
-(setenv "PATH"
-        (concat "/usr/local/go/bin" ":"
-                (concat (getenv "GOPATH") "/bin") ":"
-                (getenv "PATH")))
-
-;; The exec-path variable is a list of strings. Used by emacs itself
-(setq exec-path (append exec-path '("/usr/local/go/bin")))
-(setq exec-path (append exec-path (list (concat (getenv "GOPATH") "/bin"))))
+;; Go editing support
+(use-package go-mode
+  :ensure t
+  :mode "\\.go\\'"
+  :hook (go-mode . eglot-ensure))
 
 (add-hook 'go-mode-hook
-          '(lambda ()
-             (set (make-local-variable 'compile-command)
-                  (concat "go build -v " buffer-file-name))
-             (local-set-key (kbd "C-c C-h") 'godoc)
-             (setq indent-tabs-mode t)
-             (setq tab-width 4)
-             (set (make-local-variable 'company-backends) '(company-go))
-             (company-mode)))
+          (lambda ()
+            (setq tab-width 4)))
 
 (provide 'my-go-stuff)
 ;;; my-go-stuff.el ends here
